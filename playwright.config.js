15157ce82d60a05026d9e6cec8cfa99599842937
Satpal Jangir <satpaljangir22@gmail.com>
2024-12-08 21:16:31 +0530

    Added Hyper Execute and Gitpod yml files

--- playwright.config.js
+++ playwright.config.js
@@ -1,35 +1,5 @@
 const { defineConfig } = require("@playwright/test");
 
-//LambdaTest Chrome Capabilities
-const capabilities = {
-  browserName: "Chrome", // Browsers allowed: `Chrome`, `MicrosoftEdge`, `pw-chromium`, `pw-firefox` and `pw-webkit`
-  browserVersion: "latest",
-  "LT:Options": {
-    platform: "Windows 10",
-    build: "Playwright Test Build",
-    name: "Playwright Test",
-    user: "satpaljangir22",
-    accessKey: "VMLFdKdIB2MxcQCLlCr2i6ws1803yJVwWIvsQAtsSPJIJJcD98",
-    network: true,
-    video: true,
-    console: true,
-    tunnel: false, // Add tunnel configuration if testing locally hosted webpage
-    tunnelName: "", // Optional
-    geoLocation: "", // country code can be fetched from https://www.lambdatest.com/capabilities-generator/
-  },
-};
-
 module.exports = defineConfig({
   testDir: "tests",
-  projects: [
-    {
-      name: "Chrome on windows 10",
-      use: {
-        connectOptions: {
-          wsEndpoint: `wss://cdp.lambdatest.com/playwright?capabilities=
-          ${encodeURIComponent(JSON.stringify(capabilities))}`,
-        },
-      },
-    },
-  ],
 });
--- tests/example.test.js
+++ tests/example.test.js
@@ -11,23 +11,7 @@ afterEach(async ({ page }, testInfo) => {
   `lambdatest_action: ${JSON.stringify(setStatus)}`);
 });
 
-test("Login to LambdaTest E-Commerce", async ({ page }) => {
-  await page.goto("https://ecommerce-playground.lambdatest.io/");
-  await page.getByRole("button", { name: "My account" }).hover();
-  await page.getByRole("link", { name: "Login", exact: true }).click();
-  const title = await page.title();
-  expect.soft(title).toBe("Account Login");
-
-  await page
-    .getByPlaceholder("E-Mail Address")
-    .fill("satpaljangir22@gmail.com");
-  await page.getByPlaceholder("Password").fill("Test12345");
-  await page.getByRole("button", { name: "Login" }).click();
-  const homePageTitle = await page.title();
-  expect.soft(homePageTitle).toBe("My Account");
-});
-
-test.only("Explore LambdaTest", async ({ page }) => {
+test("Explore LambdaTest", async ({ page }) => {
   await page.goto("https://www.lambdatest.com/");
 
   const [newPage] = await Promise.all([
